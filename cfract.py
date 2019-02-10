728d1b134704d70209212dd6851402e6885b30be
aib <aibok42@gmail.com>
2019-02-10 18:18:58 +0300

    Initial version

---
+++ cfract.py
@@ -0,0 +1,114 @@
+import math
+import random
+import time
+
+import dotdict
+import numpy as np
+import pygame
+
+TAU = 2. * math.pi
+
+SIZE = (600, 600)
+DOTS = 32
+RADIUS = 250
+
+CENTER = np.array([SIZE[0] / 2, SIZE[1] / 2])
+
+def mag(v):
+	return math.sqrt(v[0]**2 + v[1]**2)
+
+def norm(v):
+	return v / mag(v)
+
+class Dot:
+	def __init__(self, pos):
+		self.pos = pos
+		self.color = (255, 0, 0)
+		self.path = []
+
+	def int_pos(self):
+		return (int(self.pos[0]), int(self.pos[1]))
+
+	def draw(self, surface):
+		for line in self.path:
+			pygame.draw.line(surface, self.color, line[0], line[1])
+		pygame.draw.circle(surface, self.color, self.int_pos(), 4)
+
+	def think(self, state, t):
+		gvec = np.array([0., 0.])
+		for dot in state.dots:
+			if dot is self:
+				continue
+
+			dpos = (dot.pos - self.pos)
+			dgrav = dpos / (mag(dpos) ** 2)
+
+			gvec += dgrav
+
+		cgrav = norm(CENTER - self.pos)
+
+		self.gravity = norm(gvec) + cgrav
+
+	def advance(self, state, t):
+		line_start = self.int_pos()
+		self.pos += norm(self.gravity) * 20 * t
+		line_end = self.int_pos()
+
+		self.path.append((line_start, line_end))
+
+
+def reset_state():
+	state = dotdict.dotdict()
+
+	state.dots = []
+	for _ in range(DOTS):
+		theta = random.random() * TAU
+		pos = np.array([CENTER[0] + RADIUS * math.cos(theta), CENTER[1] + RADIUS * math.sin(theta)])
+		dot = Dot(pos)
+		state.dots.append(dot)
+
+	return state
+
+def advance_state(state, t):
+	for dot in state.dots:
+		dot.think(state, t)
+
+	for dot in state.dots:
+		dot.advance(state, t)
+
+def draw_state(state, surface):
+	for dot in state.dots:
+		dot.draw(surface)
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((600, 600))
+	state = reset_state()
+
+	running = True
+	paused = True
+	last_advance_time = time.monotonic()
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					running = False
+				if event.key == pygame.K_SPACE:
+					state = reset_state()
+				if event.key == pygame.K_p:
+					paused = not paused
+			elif event.type == pygame.QUIT:
+				running = False
+
+		now = time.monotonic()
+		dt = now - last_advance_time
+		last_advance_time = now
+		if not paused:
+			advance_state(state, dt)
+
+		screen.fill((0, 0, 0))
+		draw_state(state, screen)
+		pygame.display.flip()
+
+if __name__ == '__main__':
+	main()
