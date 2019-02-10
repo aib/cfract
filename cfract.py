1545098d7f5209b3fc749a107f6d38cdddc3f3ed
aib <aibok42@gmail.com>
2019-02-10 18:46:13 +0300

    Add merging

--- cfract.py
+++ cfract.py
@@ -19,47 +19,70 @@ def mag(v):
 	return math.sqrt(v[0]**2 + v[1]**2)
 
 def norm(v):
-	return v / mag(v)
+	m = mag(v)
+	if m == 0:
+		return v
+	else:
+		return v / mag(v)
 
 class Dot:
 	def __init__(self, fract):
-		self.path = []
+		self.size = .5
+		self.deleted = False
 
 		theta = random.random() * TAU
 		self.pos = np.array([CENTER[0] + RADIUS * math.cos(theta), CENTER[1] + RADIUS * math.sin(theta)])
 
 		csys = colorsys.hsv_to_rgb(fract / 6, 1, 1)
-		self.color = (csys[0]*255, csys[1]*255, csys[2]*255)
+		self.color = np.array(csys) * 255
 
 	def int_pos(self):
 		return (int(self.pos[0]), int(self.pos[1]))
 
 	def draw(self, surface):
-		for line in self.path:
-			pygame.draw.line(surface, self.color, line[0], line[1])
-		pygame.draw.circle(surface, self.color, self.int_pos(), 4)
+		pygame.draw.circle(surface, self.color, self.int_pos(), max(int(self.size), 1))
 
 	def think(self, state, t):
+		if self.deleted:
+			return False
+
 		gvec = np.array([0., 0.])
 		for dot in state.dots:
-			if dot is self:
+			if dot is self or dot.deleted:
 				continue
 
 			dpos = (dot.pos - self.pos)
-			dgrav = dpos / (mag(dpos) ** 2)
+			dist = mag(dpos)
 
+			if dist < (self.size + dot.size)/2:
+				self.merge(dot)
+				continue
+
+			dgrav = dpos / (dist ** 2)
 			gvec += dgrav
 
-		cgrav = norm(CENTER - self.pos)
+		dcenter = CENTER - self.pos
+		if mag(dcenter) < self.size:
+			self.deleted = True
+
+		cgrav = norm(dcenter)
 
 		self.gravity = norm(gvec) + cgrav
+		return True
 
 	def advance(self, state, t):
 		line_start = self.int_pos()
 		self.pos += norm(self.gravity) * 20 * t
 		line_end = self.int_pos()
 
-		self.path.append((line_start, line_end))
+		state.paths.append(dotdict.dotdict(start=line_start, end=line_end, color=self.color, width=self.size))
+
+	def merge(self, other):
+		total_size = self.size + other.size
+
+		self.color = (self.color*self.size + other.color*other.size) / total_size
+		self.size = (self.size/2 + other.size/2) * math.sqrt(3)
+		other.deleted = True
 
 
 def reset_state():
@@ -70,16 +93,24 @@ def reset_state():
 		dot = Dot(float(i) / DOTS)
 		state.dots.append(dot)
 
+	state.paths = []
+
 	return state
 
 def advance_state(state, t):
+	new_dots = []
 	for dot in state.dots:
-		dot.think(state, t)
+		alive = dot.think(state, t)
+		if alive:
+			new_dots.append(dot)
+	state.dots = new_dots
 
 	for dot in state.dots:
 		dot.advance(state, t)
 
 def draw_state(state, surface):
+	for line in state.paths:
+		pygame.draw.line(surface, line.color, line.start, line.end, max(int(line.width*2), 1))
 	for dot in state.dots:
 		dot.draw(surface)
 
